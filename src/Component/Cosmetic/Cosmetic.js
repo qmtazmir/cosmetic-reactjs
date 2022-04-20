23bb17b041d3a067a1ee56822ce7be7a75fbf39e
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-04-20 19:31:45 +0600

    data genaret

--- src/Component/Cosmetic/Cosmetic.js
+++ src/Component/Cosmetic/Cosmetic.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Cosmetic = (props) => {
-    
-    return (
-        <div>
-            <h3>By this : { }</h3>
-            <p>Only for : $</p>
-        </div>
-    );
+  const { name, price, id } = props.cosmetic;
+
+  return (
+    <div>
+      <h3>Product id: {id}</h3>
+      <h3>By this : {name}</h3>
+      <p>Only for :{price}$</p>
+    </div>
+  );
 };
 
 export default Cosmetic;
--- src/Component/Cosmetics/Cosmetics.js
+++ src/Component/Cosmetics/Cosmetics.js
@@ -4,39 +4,34 @@ import Cosmetic from "../Cosmetic/Cosmetic";
 const Cosmetics = () => {
   const cosmetics = [
     {
-      id: 570,
-      name: "Alta",
-      price: 120,
+      id: "62600aee1ffafb0bd93e9064",
+      price: 174,
+      name: "Terrie Marsh",
     },
     {
-      id: 670,
-      name: "Nailpalish",
-      price: 150,
+      id: "62600aee92c884723e8e8798",
+      price: 166,
+      name: "Evangeline Talley",
     },
     {
-      id: 770,
-      name: "Face Powder",
-      price: 300,
+      id: "62600aeef8173225a994784b",
+      price: 136,
+      name: "Tami Le",
     },
     {
-      id: 870,
-      name: "Ilana",
-      price: 220,
+      id: "62600aeebc80bd55e62c9182",
+      price: 539,
+      name: "Sheree Carrillo",
     },
     {
-      id: 970,
-      name: "Mashkara",
-      price: 320,
+      id: "62600aeec87646570f23e631",
+      price: 586,
+      name: "Josefina Davidson",
     },
     {
-      id: 470,
-      name: "Shope",
-      price: 130,
-    },
-    {
-      id: 370,
-      name: "Churi",
-      price: 120,
+      id: "62600aee8e6a4e022e7eacda",
+      price: 134,
+      name: "Rogers Stout",
     },
   ];
   return (
